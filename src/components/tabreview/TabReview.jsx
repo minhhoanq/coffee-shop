be2ebdc7c05a12ed0e625eac19474ca57faff952
minhhoanq <mh.tranminhhoang@gmail.com>
2023-06-22 23:45:09 +0700

    #Reviews, Similar Product

---
+++ src/components/tabreview/TabReview.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+import './tab-review.scss';
+import { useState } from "react";
+import Button from "../button/Button";
+
+const TabReview = props => {
+
+    const[tab, setTab] = useState('desc');
+
+    const item = props.item;
+
+    return (
+        <div className="tab-review">
+            <div className={"tab-review__wrapper"} >
+                <span className={`tab-review__wrapper__desc  ${tab === 'desc' ? 'active' : ''}`} onClick={() => setTab('desc')}>
+                    Description
+                </span>
+
+                <span className={`tab-review__wrapper__reviews ${tab === 'reviews' ? 'active' : ''}`} onClick={() => setTab('reviews')}>
+                    Reviews (2)
+                </span>
+            </div>
+
+            {tab === 'desc' ? (<TabDescription description={item.description}/>) : (<TabReviews reviews={item.reviews}/>)}
+        </div>
+    )
+}
+
+export const TabDescription = props => {
+
+    return (
+        <div className="tab-description">
+            <span>{props.description}</span>
+        </div>
+    )
+}
+
+export const TabReviews = props => {
+
+    const reviews = props.reviews;
+
+    return (
+        <div className="tab-reviews">
+
+            {reviews.map((review, i) => (
+                <div className="tab-reviews__users" key={i}>
+                    <span className="tab-reviews__users__name">John Canvas</span>
+
+                    <span className="tab-reviews__users__rate">{review.rating} (rating)</span>
+
+                    <span className="tab-reviews__users__comment">{review.text}</span>
+                </div>
+            ))}
+
+            <div className="tab-reviews__write">
+                <span className="tab-reviews__write__title">
+                    Leave your experience
+                </span>
+
+                <div className="tab-reviews__write__rating">
+                    <i class="ri-star-s-fill">1</i>
+                    <i class="ri-star-s-fill">2</i>
+                    <i class="ri-star-s-fill">3</i>
+                    <i class="ri-star-s-fill">4</i>
+                    <i class="ri-star-s-fill">5</i>
+                </div>
+
+                <div className="tab-reviews__write__wrapper">
+                    <textarea className="tab-reviews__write__wrapper__content"></textarea>
+                </div>
+
+                <Button>Submit</Button>
+            </div>
+        </div>
+    )
+}
+
+export default TabReview;
--- src/pages/detailproduct/DetailProduct.jsx
+++ src/pages/detailproduct/DetailProduct.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 import './detail-product.scss';
 import PageHeader from '../../components/pageheader/PageHeader';
 import Button from '../../components/button/Button';
 import { useParams } from "react-router-dom";
 import products from '../../assets/data/products';
+import TabReview from "../../components/tabreview/TabReview";
+import ProductList from '../../components/productlist/ProductList';
 
 const DetailProduct = () => {
+
+    const[productSimilars, setProductSimilars] = useState([]);
 
     const { id } = useParams();
 
@@ -17,6 +21,13 @@ const DetailProduct = () => {
             value = item;
         }
     });
+
+    useEffect(() => {
+        const listSimilars = products.filter((item) => item.category === value.category);
+        setProductSimilars(listSimilars);
+    },[]);
+
+    console.log(productSimilars);
 
     return (
         <div className="detail-product">
@@ -64,6 +75,15 @@ const DetailProduct = () => {
 
                     <Button className="detail-product__wrapper__info__btn">Add to cart</Button>
                 </div>
+            </div>
+
+            <div className="detail-product__review">
+                <TabReview item={value}/>
+            </div>
+
+            <div className="detail-product__similar">
+                <span className="detail-product__similar__title">You might also like</span>
+                <ProductList items={productSimilars}/>
             </div>
         </div>
     );
