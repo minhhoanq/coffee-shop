e2aba178ac6571ffc2c99106f200958d1aab5ea2
minhhoanq <mh.tranminhhoang@gmail.com>
2023-07-12 22:38:06 +0700

    #Repair Responsive Header

--- Client/src/components/header/Header.jsx
+++ Client/src/components/header/Header.jsx
@@ -71,97 +71,96 @@ const Header = () => {
     }
 
     return (
-        <div className="header" ref={headerRef}>
-            <div className="header__wrapper container">
-                <Link to={'/'} className="header__wrapper__logo">
-                    <i class="ri-cup-line"></i>
-                    Coffee Shop
-                </Link>
-
-                <ul className="header__wrapper__nav">
-                    {headerNav.map((item, i) => (
-                        <li key={i} className={`${i === active ? 'active' : ''}`}>
-                            <Link to={item.path}>
-                                {item.display}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-
-                <div className="header__wrapper__options">
-                    <div className="header__wrapper__options__notify" onClick={handleNotify}>
-                        <i class="ri-heart-line" ></i>
-                        <div className="header__wrapper__options__notify__dot"></div>
-
-                        <div className="header__wrapper__options__notify__list" ref={notifyListRef}>
-                            <div className="header__wrapper__options__notify__list__txt">Thông báo mới nhận</div>
-
-                            <ul className="header__wrapper__options__notify__list__ul">
-                                <li className="header__wrapper__options__notify__list__ul__li">
-                                    <Car_ver/>
-                                </li>
-
-                                <li className="header__wrapper__options__notify__list__ul__li">
-                                    <Car_ver/>
-                                   
-                                </li>
-
-                                <li className="header__wrapper__options__notify__list__ul__li">
-                                    <Car_ver/>
-                                    
-                                </li>
-
-                                <li className="header__wrapper__options__notify__list__ul__li">
-                                    <Car_ver/>
-                                </li>
-
-                                <li className="header__wrapper__options__notify__list__ul__li">
-                                    <Car_ver/>
-                                </li>
-                            </ul>
-
-                            <button className="header__wrapper__options__notify__list__btn">Xem tất cả</button>
-                        </div>
-                    </div>
-                    <div className="header__wrapper__options__cart">
-                        <i class="ri-shopping-cart-line"></i>
-                    </div>
-                    <div className="header__wrapper__options__profile">
-                        {
-                            user ? <>
-                                <span>Hi,{user.username}</span>
-                                <span style={{cursor: "pointer"}} onClick={handleLogout}>Logout</span>
-                            </> : <i class="ri-user-smile-line"></i>
-                        }
-                    </div>
-                </div>
-
-                <button className="header__wrapper__mobile"  >
-                    <i class="ri-menu-line" onClick={toggleHandleMenuShow}></i>
-                    <div className="header__wrapper__mobile__menu" ref={navMobileRef}>
-                        <i class="ri-arrow-right-circle-line" onClick={toggleHandleMenuShow}></i>
-                        <ul>
-                            {headerNav.map((item, i) => (
+            <div className="header"  ref={headerRef}>
+                <div className="header__wrapper wide col">
+                    <Link to={'/'} className="header__wrapper__logo">
+                        <i class="ri-cup-line"></i>
+                        Coffee Shop
+                    </Link>
+    
+                    <ul className="header__wrapper__nav">
+                        {headerNav.map((item, i) => (
                             <li key={i} className={`${i === active ? 'active' : ''}`}>
                                 <Link to={item.path}>
                                     {item.display}
                                 </Link>
                             </li>
-                            ))}
-
-                            <li className="header__wrapper__mobile__menu__btn">
-                                Notifycation
-                            </li>
-                            <li className="header__wrapper__mobile__menu__btn">
-                                Profile
-                            </li>
-                        </ul>
+                        ))}
+                    </ul>
+    
+                    <div className="header__wrapper__options">
+                        <div className="header__wrapper__options__notify" onClick={handleNotify}>
+                            <i class="ri-heart-line" ></i>
+                            <div className="header__wrapper__options__notify__dot"></div>
+    
+                            <div className="header__wrapper__options__notify__list" ref={notifyListRef}>
+                                <div className="header__wrapper__options__notify__list__txt">Thông báo mới nhận</div>
+    
+                                <ul className="header__wrapper__options__notify__list__ul">
+                                    <li className="header__wrapper__options__notify__list__ul__li">
+                                        <Car_ver/>
+                                    </li>
+    
+                                    <li className="header__wrapper__options__notify__list__ul__li">
+                                        <Car_ver/>
+                                       
+                                    </li>
+    
+                                    <li className="header__wrapper__options__notify__list__ul__li">
+                                        <Car_ver/>
+                                        
+                                    </li>
+    
+                                    <li className="header__wrapper__options__notify__list__ul__li">
+                                        <Car_ver/>
+                                    </li>
+    
+                                    <li className="header__wrapper__options__notify__list__ul__li">
+                                        <Car_ver/>
+                                    </li>
+                                </ul>
+    
+                                <button className="header__wrapper__options__notify__list__btn">Xem tất cả</button>
+                            </div>
+                        </div>
+                        <div className="header__wrapper__options__cart">
+                            <i class="ri-shopping-cart-line"></i>
+                        </div>
+                        <div className="header__wrapper__options__profile">
+                            {
+                                user ? <>
+                                    <span>Hi,{user.username}</span>
+                                    <span style={{cursor: "pointer"}} onClick={handleLogout}>Logout</span>
+                                </> : <i class="ri-user-smile-line"></i>
+                            }
+                        </div>
                     </div>
-                    <div className="header__wrapper__mobile__overlay" ref={overLayMobileRef}></div>
-                </button>
+    
+                    <button className="header__wrapper__mobile"  >
+                        <i class="ri-menu-line" onClick={toggleHandleMenuShow}></i>
+                        <div className="header__wrapper__mobile__menu" ref={navMobileRef}>
+                            <i class="ri-arrow-right-circle-line" onClick={toggleHandleMenuShow}></i>
+                            <ul>
+                                {headerNav.map((item, i) => (
+                                <li key={i} className={`${i === active ? 'active' : ''}`}>
+                                    <Link to={item.path}>
+                                        {item.display}
+                                    </Link>
+                                </li>
+                                ))}
+    
+                                <li className="header__wrapper__mobile__menu__btn">
+                                    Notifycation
+                                </li>
+                                <li className="header__wrapper__mobile__menu__btn">
+                                    Profile
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="header__wrapper__mobile__overlay" ref={overLayMobileRef}></div>
+                    </button>
+                </div>
             </div>
-            
-        </div>
     );
 }
 
