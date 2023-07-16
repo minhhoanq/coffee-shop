42f54b6498c964f2c78b8e43f608a19031c9a36f
minhhoanq <mh.tranminhhoang@gmail.com>
2023-07-16 22:31:38 +0700

    #1.1 CSS SideBar ADMIN

--- Client/src/components/sidebar/MenuSideBarItem.jsx
+++ Client/src/components/sidebar/MenuSideBarItem.jsx
@@ -5,8 +5,8 @@ const MenuSideBarItem = props => {
     const item = props.item;
     return (
         <NavLink 
-            className={(nav) => (`menu-side-bar-item ${( {active: nav.isActive})}`)}
-            // to={item.pathname}
+            className={(`menu-side-bar-item`)}
+            to={item.pathname}
         >
             {item.title}
         </NavLink>
