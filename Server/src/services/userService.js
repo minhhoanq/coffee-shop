fad5b3b413de9bc6f71dd7874e137e68ac4aa269
minhhoanq <mh.tranminhhoang@gmail.com>
2023-07-21 20:59:03 +0700

    #API Delete User by Id

--- Server/src/services/userService.js
+++ Server/src/services/userService.js
@@ -19,4 +19,30 @@ const getAllStaffService = () => new Promise( async(resolve, reject) => {
     }
 });
 
-module.exports = { getAllStaffService };
+const updateUserByidService = ({ id }) => new Promise(async(resolve, reject) => {
+    try {
+        const response = await db.User.update({email, username, password, firstname, lastname, image, roles, sex, phone, birth, address, isDelete})
+    } catch (error) {
+        reject(error);
+    }
+})
+
+const deleteUserbyIdService = ({ id }) => new Promise( async(resolve, reject) => {
+    try {
+        const response = await db.User.destroy({
+            where: {
+                id,
+            },
+        });
+
+        resolve({
+            err: response ? 0 : 1,
+            mes: response ? "success!" : "Can't found staff",
+            staffData: id,
+        })
+    } catch (error) {
+        reject(error)
+    }
+});
+
+module.exports = { getAllStaffService, deleteUserbyIdService };
