e972d4c6ec89d5a60871539c6aa1649eb36bc2fc
minhhoanq <mh.tranminhhoang@gmail.com>
2023-09-06 22:43:03 +0700

    #2 CSS Payment Page FE

--- Client/src/pages/payment/Payment.jsx
+++ Client/src/pages/payment/Payment.jsx
@@ -2,16 +2,47 @@ import React from "react";
 
 import './payment.scss';
 
+import Modal, { ModalContent } from '../../components/modal/Modal';
+
 const Payment = () => {
+
+    const showModalAddBank = (e) => {
+        e.preventDefault();
+
+        const modal = document.querySelector('#modal');
+        modal.classList.add('active');
+    }
+
+    const closeModalAddBank = (e) => {
+        e.preventDefault();
+        
+        const modal = document.querySelector('#modal');
+        modal.classList.remove('active');
+    }
+
     return (
         <div className="payment">
         <div className="payment__header">
             <span className="payment__header__title">Tài khoản ngân hàng của tôi</span>
 
-            <button className="payment__header__btn">
+            <button className="payment__header__btn" onClick={showModalAddBank}>
                 <i class="ri-add-line"></i>
                 Thêm tài khoản ngân hàng
             </button>
+
+            <Modal id={'modal'} className={'modal'}>
+                <ModalContent className={'modal__content'}>
+                    Thông tin người dùng
+
+                    <input type="text" className="modal__content__name" placeholder="Họ tên"/>
+                    <input type="text" className="modal__content__cccd" placeholder="Số CCCD"/>
+
+                    <div className="modal__content__wrapper">
+                        <button className="modal__content__wrapper__cancel" onClick={closeModalAddBank}>Trở lại</button>
+                        <button className="modal__content__wrapper__submit">Xác nhận</button>
+                    </div>
+                </ModalContent>
+            </Modal>
         </div>
         <div className="payment__wrapper">
             <ul className="payment__wrapper__form__w">
