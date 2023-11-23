e5a3ff31042f773248fae01febddb9a277c2aed0
minhhoanq <mh.tranminhhoang@gmail.com>
2023-11-23 16:13:06 +0700

    #2 Dashboard Page FE

--- Client/src/components/common/CardContentCpn.jsx
+++ Client/src/components/common/CardContentCpn.jsx
@@ -1,34 +1,32 @@
 import { Box, Card, CardActions, CardContent, Icon, IconButton, ListItemIcon, Stack, Typography, colors } from "@mui/material";
 
-import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
-import ArrowUpwardOutlinedIcon from '@mui/icons-material/ArrowUpwardOutlined';
 
-const CardContentCpn = () => {
+const CardContentCpn = props => {
+    const item = props.item;
     return (
         <Card sx={{
-            width: "300px",
-            height: "150px"
+            // width: "250px",
+            // height: "150px"
         }}>
             <CardContent>
                 <Stack spacing={1} alignItems={"flex-start"}>
-                    <IconButton size="large">
-                        <RemoveRedEyeOutlinedIcon fontSize="large" color="primary"/>
+                    <IconButton size="large" color="info">
+                        {item.icon}
                     </IconButton>
                     <Typography variant="h5" fontWeight={600}>
-                        251
+                        {item.number}
                     </Typography>
                     <Stack direction={"row"} justifyContent={"space-between"} alignItems={"center"} width={"100%"}>
                         <Typography fontWeight={600} color={"#999"}>
-                            Total views
+                            Total {item.content}
                         </Typography>
 
                        <Box direction={"row"} display={"flex"} alignItems={"center"}>
-                            <Typography color={colors.green[500]}>
-                                0.43%
+                            <Typography color={item.color}>
+                                {item.percent}%
                             </Typography>
-                                <ArrowUpwardOutlinedIcon color="success" fontSize="small"/>
+                            {item.iconPercent}
                        </Box>
-                       aaaa
                     </Stack>
                 </Stack>
             </CardContent>
--- Client/src/pages/admin/Dashboard.jsx
+++ Client/src/pages/admin/Dashboard.jsx
@@ -1,11 +1,131 @@
-import { Box } from "@mui/material";
+import { Box, Grid, Paper, Stack } from "@mui/material";
+import { LineChart } from '@mui/x-charts/LineChart';
+import { BarChart } from '@mui/x-charts/BarChart';
+import { PieChart } from '@mui/x-charts/PieChart';
 
-import CardContentCpn from "../../components/common/CardContentCpn"
+import CardContentCpn from "../../components/common/CardContentCpn";
+import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
+import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
+import ArrowUpwardOutlinedIcon from '@mui/icons-material/ArrowUpwardOutlined';
+import ArrowDownwardOutlinedIcon from '@mui/icons-material/ArrowDownwardOutlined';
+
+const CardContents = [
+    {
+        icon: <RemoveRedEyeOutlinedIcon/>,
+        number: 2.873,
+        content: "View",
+        percent: 0.43,
+        color: "green",
+        iconPercent: <ArrowUpwardOutlinedIcon color="success"/>
+    },
+    {
+        icon: <ShoppingCartOutlinedIcon/>,
+        number: 5.353,
+        content: "Profit",
+        percent: 0.43,
+        color: "green",
+        iconPercent: <ArrowUpwardOutlinedIcon color="success"/>
+    },
+    {
+        icon: <Inventory2OutlinedIcon/>,
+        number: 873,
+        content: "Product",
+        percent: 0.63,
+        color: "green",
+        iconPercent: <ArrowUpwardOutlinedIcon color="success"/>
+    },
+    {
+        icon: <GroupsOutlinedIcon/>,
+        number: 273,
+        content: "Users",
+        percent: 1.39,
+        color: "red",
+        iconPercent: <ArrowDownwardOutlinedIcon color="error"/>
+    }
+]
+
+const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
+const xLabels = [
+  'Page A',
+  'Page B',
+  'Page C',
+  'Page D',
+  'Page E',
+  'Page F',
+  'Page G',
+];
 
 const Dashboard = () => {
+
+
     return (
-        <Box p={4}>
-            <CardContentCpn/>
+        <Box display={"flex"} justifyContent={"center"} p={4} bgcolor={"#f5f5f5"}>
+            <Grid container xs={12}>
+                <Grid container spacing={4}>
+                    {CardContents && CardContents.map((item, index) => (
+                        <Grid item lg={3} md={4} sm={6} xs={12} key={index}>
+                            <CardContentCpn item={item}/>
+                        </Grid>
+                    ))}
+                </Grid>
+
+                <Stack justifyContent={"space-between"} width={"100%"} mt={4} direction={"row"}>
+                    <Paper width={"100%"}>
+                        <LineChart
+                            width={700}
+                            height={300}
+                            series={[
+                                { data: pData, label: 'pv', yAxisKey: 'leftAxisId' },
+                                { data: uData, label: 'uv', yAxisKey: 'rightAxisId' },
+                            ]}
+                            xAxis={[{ scaleType: 'point', data: xLabels }]}
+                            yAxis={[{ id: 'leftAxisId' }, { id: 'rightAxisId' }]}
+                            rightAxis="rightAxisId"
+                        />
+                    </Paper>
+                    <Paper>
+                        <BarChart
+                            series={[ 
+                                { data: [3, 4, 1], stack: 'A', label: 'Series A1' },
+                                { data: [4, 3, 1], stack: 'A', label: 'Series A2' },
+                                { data: [4, 2, 5], stack: 'B', label: 'Series B1' },
+                                { data: [2, 8, 1], stack: 'B', label: 'Series B2' },
+                            ]}
+                            width={400}
+                            height={300}
+                        />
+                    </Paper>
+                </Stack>
+                <Stack justifyContent={"space-between"} width={"100%"} direction={"row"} mt={4} spacing={4}>
+                    <Box flex={1}>
+                        <Paper>
+                            <PieChart
+                                series={[
+                                    {
+                                    data: [
+                                        { id: 0, value: 10, label: 'series A' },
+                                        { id: 1, value: 15, label: 'series B' },
+                                        { id: 2, value: 20, label: 'series C' },
+                                    ],
+                                    },
+                                ]}
+                                width={400}
+                                height={200}
+                            />
+                        </Paper>
+                    </Box>
+                    <Box flex={2} >
+                        <Paper> 
+                            <Box height={"200px"}>
+                                Loading...
+                            </Box>
+                        </Paper>
+                    </Box>
+                </Stack>
+            </Grid>
         </Box>
     )
 }
