eaff9dacc12b4b498f51954ae89d8af3a409ac11
minhhoanq <mh.tranminhhoang@gmail.com>
2024-01-23 15:49:11 +0700

    #Add morgan, helmet, compression BE

---
+++ Server/server.js
@@ -0,0 +1,13 @@
+const app = require("./src/index");
+
+const PORT = 5000;
+
+const server = app.listen( PORT, () => {
+    console.log(`Start with ${PORT}`)
+})
+
+process.on('SIGINT', () => {
+    server.close(() => {
+        console.log('Exit server express');
+    })
+})
--- Server/src/index.js
+++ Server/src/index.js
@@ -2,12 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
-const db = require("./src/config/db");
-const initRoutes = require("./src/routes");
+const db = require("./config/db");
+const initRoutes = require("./routes");
+const morgan = require("morgan");
+const { default: helmet } = require("helmet");
+const compression = require("compression");
 
 dotenv.config();
 const app = express();
 
+// init middleware
+app.use(morgan("dev"))
+app.use(helmet())
+app.use(compression())
+
+// init db
 db();
 app.use(cors({
     origin: "http://localhost:3000" ,
@@ -16,8 +25,8 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 
+//init router
 initRoutes(app);
+// init error
 
-app.listen(5000, () => {
-    console.log("Server is runing")
-})
+module.exports = app
